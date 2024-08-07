65993ad1b04364bce00253d70033e91091382f18
alice <alicelouisew95@gmail.com>
2024-08-07 14:54:19 +0200

    Initial Commit

---
+++ SwiftySudoku/SwiftySudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftySudokuApp.swift
+//  SwiftySudoku
+//
+//  Created by Alice Wheeler on 2024-08-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftySudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
